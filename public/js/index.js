66b2d0334a171f156afa670c7b8290af6c914685
suni <sunilkumarachra9461@gmail.com>
2021-10-13 14:48:33 +0530

    2:46 13-10

--- public/js/index.js
+++ public/js/index.js
@@ -2,13 +2,30 @@ var myNav = document.getElementById('mynav');
 window.onscroll = function () { 
     "use strict";
     if (document.documentElement.scrollTop>= 50 ) {
-    console.log("hii");
 
         myNav.classList.add("nav-colored");
         myNav.classList.remove("nav-transparent");
+        myNav.innerHTML=`   <ul>
+        <li><a href="#">HOME</a></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+    <div class="line"></div>
+    `;
     } 
     else {
         myNav.classList.add("nav-transparent");
         myNav.classList.remove("nav-colored");
+        myNav.innerHTML=`   <ul>
+        <li><a href="#">HOME</a></li>
+        <li><a href="#">COLLEGES</a></li>
+        <li><a href="#">NIRF</a></li>
+        <li><a href="#">REVIEWS</a></li>
+        <li><a href="#">CONTACT US</a></li>
+        
+    </ul>
+    <div class="line"></div>`;
     }
 };
